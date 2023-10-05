1e15f9cde10253074aa210e12cedbeba950e37d9
Marcello Lopes <marcellolopesdev@gmail.com>
2023-10-05 17:44:27 -0300

    home layout cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center">
       <div className="w-full max-w-[800px] mx-auto">
-        <h1 className="text-6xl mb-4">
-          AI Mood Journal. <br /> Track how your days feel.
+        <h1 className="text-6xl mb-8">
+          <span className="text-indigo-400">AI Mood Journal.</span> <br /> Track
+          how your days feel.
         </h1>
-        <p className="text-2xl text-white/60 mb-4">Be honest {'<3'}</p>
+        <p className="text-2xl text-white/60 mb-16">Be honest {'<3'}</p>
         <div>
           <Link href="/journal">
             <button className="bg-indigo-600 px-4 py-2 rounded-lg text-xl">
