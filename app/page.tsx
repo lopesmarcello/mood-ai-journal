7116008a4043f09d942f07f540b613f5ca187bd5
Marcello Lopes <marcellolopesdev@gmail.com>
2023-10-06 17:00:01 -0300

    basic auth/db setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
     <div className="w-screen h-screen bg-black flex justify-center items-center">
       <div className="w-full max-w-[800px] mx-auto">
         <h1 className="text-6xl mb-8">
-          <span className="text-indigo-400">AI Mood Journal.</span> <br /> Track
-          how your days feel.
+          <span className="text-indigo-400 font-bold">AI Mood Journal.</span>{' '}
+          <br /> Track how your days feel.
         </h1>
-        <p className="text-2xl text-white/60 mb-16">Be honest {'<3'}</p>
+        <p className="text-2xl text-white/80 mb-16">
+          Be honest <span className="text-red-400 font-bold">{'<3'}</span>
+        </p>
         <div>
           <Link href="/journal">
             <button className="bg-indigo-600 px-4 py-2 rounded-lg text-xl">
---
+++ utils/db.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+
+// Prisma Setup segundo a Documentação planetscale:
+// const prisma = new PrismaClient()
+
+// async function main() {
+//   // ... you will write your Prisma Client queries here
+// }
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
